d6f40d644aa7ee8d1eaa83d329c0b5387d3628df
Vasilkova Lyubov <damirana_@mail.ru>
2025-09-14 23:54:07 +0700

    saving a customization option for the database.py file using annotations

--- app/database.py
+++ app/database.py
@@ -1,23 +1,23 @@
+from datetime import datetime
+from typing import Annotated
+
+from sqlalchemy import func
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncAttrs
-from sqlalchemy.orm import DeclarativeBase, declared_attr
+from sqlalchemy.orm import DeclarativeBase, declared_attr, Mapped, mapped_column
+
 from app.config import get_db_url
 
-
 DATABASE_URL = get_db_url()
+
 engine = create_async_engine(DATABASE_URL)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
-# DB_HOST = 'localhost'
-# DB_PORT = '5432'
-# DB_NAME = 'fast_api'
-# DB_USER = 'zlayazayaz'
-# DB_PASSWORD = 'zlayazayaz'
-#
-# DATABASE_URL = f'postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
-#
-# engine = create_async_engine(DATABASE_URL)
-# async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
-#
+# настройка аннотаций
+int_pk = Annotated[int, mapped_column(primary_key=True)]
+created_at = Annotated[datetime, mapped_column(server_default=func.now())]
+updated_at = Annotated[datetime, mapped_column(server_default=func.now(), onupdate=datetime.now)]
+str_uniq = Annotated[str, mapped_column(unique=True, nullable=False)]
+str_null_true = Annotated[str, mapped_column(nullable=True)]
 
 
 class Base(AsyncAttrs, DeclarativeBase):
@@ -26,3 +26,6 @@ class Base(AsyncAttrs, DeclarativeBase):
     @declared_attr.directive
     def __tablename__(cls) -> str:
         return f"{cls.__name__.lower()}s"
+
+    created_at: Mapped[created_at]
+    updated_at: Mapped[updated_at]
